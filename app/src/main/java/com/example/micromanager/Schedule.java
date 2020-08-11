40d98722485f6f53185bb39396a9a0504dc655c5
safwaant <62581576+safwaant@users.noreply.github.com>
2020-08-10 22:03:36 -0700

    Added sharedprefs to read from and schedule generation algorithm method header

--- app/src/main/java/com/example/micromanager/Schedule.java
+++ app/src/main/java/com/example/micromanager/Schedule.java
@@ -8,21 +8,28 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Schedule extends AppCompatActivity {
-
+    public List<Assignment> assignments = new ArrayList<Assignment>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
         SharedPreferences prefs = getApplicationContext().getSharedPreferences("prefs", Context.MODE_PRIVATE);
-
+        String name = prefs.getString(getString(R.string.nameText), " ");
+        String type = prefs.getString(getString(R.string.typeText),"");
+        String dueDate = prefs.getString(getString(R.string.dueDateText), "");
+        createAssignmentAndAddToList(name, type, dueDate);
 
     }
 
-
-
+    public void createAssignmentAndAddToList(String name, String type, String dueDate){
+        assignments.add(new Assignment(name, type, dueDate));
+        Log.d("Current List: ", assignments.toString());
+    }
     private void scheduleGenerationAlgorithm(List<Assignment> assignmentList){
 
     }
