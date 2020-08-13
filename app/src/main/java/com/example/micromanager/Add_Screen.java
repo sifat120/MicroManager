500c7615a51e2d79659d267437809e4bd97c9ed3
safwaant <62581576+safwaant@users.noreply.github.com>
2020-08-12 21:19:47 -0700

    Added debugging for shared preferences and intents to pass the number of keys

--- app/src/main/java/com/example/micromanager/Add_Screen.java
+++ app/src/main/java/com/example/micromanager/Add_Screen.java
@@ -8,12 +8,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class Add_Screen extends AppCompatActivity {
+    public int keyNum = 0;
     SharedPreferences.Editor editor;
     SharedPreferences localPrefs;
     EditText nameField, dueDateField, typeField;
@@ -44,9 +46,14 @@ public class Add_Screen extends AppCompatActivity {
         due_date = dueDateField.getText().toString();
 
         editor = localPrefs.edit();
-        editor.putString(getString(R.string.nameText), name);
-        editor.putString(getString(R.string.typeText), type);
-        editor.putString(getString(R.string.dueDateText), due_date);
+        ++keyNum;
+        String nameKey = getString(R.string.nameText) + keyNum;
+        String typeKey = getString(R.string.typeText) + keyNum;
+        String dueDateKey = getString(R.string.dueDateText) + keyNum;
+        editor.putString(nameKey, name);
+        editor.putString(typeKey, type);
+        editor.putString(dueDateKey, due_date);
+        Log.d("NAME TYPE DUE DATE", nameKey + " " + typeKey + " "+ dueDateKey);
         editor.apply();
 
         nameField.getText().clear();
@@ -60,6 +67,7 @@ public class Add_Screen extends AppCompatActivity {
 
     public void goToScheduleScreen(View view) {
         Intent intent = new Intent(this, Schedule.class);
+        intent.putExtra("numOfKeys", keyNum);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/micromanager/Schedule.java
+++ app/src/main/java/com/example/micromanager/Schedule.java
@@ -19,21 +19,29 @@ public class Schedule extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
         SharedPreferences prefs = getApplicationContext().getSharedPreferences("prefs", Context.MODE_PRIVATE);
-        String name = prefs.getString(getString(R.string.nameText), " ");
-        String type = prefs.getString(getString(R.string.typeText),"");
-        String dueDate = prefs.getString(getString(R.string.dueDateText), "");
-        createAssignmentAndAddToList(name, type, dueDate);
+        Intent intent = getIntent();
+        int keyNum = intent.getIntExtra("numOfKeys",0);
+        Log.d("KEY NUM IS", "" + keyNum);
+        for(int i = 1; i <= keyNum; i++){
+            String name = prefs.getString(getString(R.string.nameText) + i, "");
+            String type = prefs.getString(getString(R.string.typeText) + i,"");
+            String dueDate = prefs.getString(getString(R.string.dueDateText)+ i, "");
+            Log.d("NAME, TYPE, DUE DATE", name +" " + type+ " "+ dueDate);
+            createAssignmentAndAddToList(name, type, dueDate);
+        }
 
-    }
-
-    public void createAssignmentAndAddToList(String name, String type, String dueDate){
-        assignments.add(new Assignment(name, type, dueDate));
         String test = "";
         for(int i = 0; i < assignments.size();i++){
             test+=assignments.get(i).toString();
         }
         Log.d("Current List: ", test);
     }
+
+    public void createAssignmentAndAddToList(String name, String type, String dueDate){
+        assignments.add(new Assignment(name, type, dueDate));
+
+    }
+
     private void scheduleGenerationAlgorithm(List<Assignment> assignmentList){
 
     }
