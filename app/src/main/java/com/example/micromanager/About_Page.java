256c43f6cca1f8961af5c0526a8e50edf16ce310
safwaant <safwaant786@gmail.com>
2020-10-01 22:41:14 -0700

    Updated about us page

--- app/src/main/java/com/example/micromanager/About_Page.java
+++ app/src/main/java/com/example/micromanager/About_Page.java
@@ -1,6 +1,8 @@
 package com.example.micromanager;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +12,11 @@ public class About_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about__page);
+
+        Toolbar registrationToolbar = findViewById(R.id.backtohomeBar);
+        setSupportActionBar(registrationToolbar);
+        ActionBar ab = getSupportActionBar();
+        assert ab != null;
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
